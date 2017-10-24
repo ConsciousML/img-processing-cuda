518900dc9867db5133c65dcd465d508e05ed0a69
Axel Mendoza <axel.mendoza@epita.fr>
2017-10-24 14:17:39 +0200

    add weight-decay to nlm parameters

--- src/cpu/non_local_means_cpu.cpp
+++ src/cpu/non_local_means_cpu.cpp
@@ -29,28 +29,28 @@ std::valarray<float> conv(Mat image, int y, int x, int conv_size)
     return rgb;
 }
 
-std::valarray<float> gauss_weight(Mat image, int y1, int x1, int y2, int x2, int conv_size, double stddev)
+std::valarray<float> gauss_weight(Mat image, int y1, int x1, int y2, int x2, int conv_size, double stddev, float weight_decay)
 {
     auto b1 = conv(image, y1, x1, conv_size);
     auto b2 = conv(image, y2, x2, conv_size);
     std::valarray<float> res = {0, 0, 0};
-    res = std::exp(-(std::pow(std::abs(b1 - b2), 2) / std::pow(stddev * 2.5, 2)));
+    res = std::exp(-(std::pow(std::abs(b1 - b2), 2) / (std::pow(stddev, 2) * weight_decay)));
     return res;
 }
 
- std::valarray<float> cp(Mat image, int y, int x, int conv_size, double stddev)
+ std::valarray<float> cp(Mat image, int y, int x, int conv_size, double stddev, float weight_decay)
 {
     std::valarray<float> sum = {0, 0, 0};
     for (int j = 0; j < image.cols; j++)
         for (int i = 0; i < image.rows; i++)
         {
-            auto tmp = gauss_weight(image, y, x, j, i, conv_size, stddev);
+            auto tmp = gauss_weight(image, y, x, j, i, conv_size, stddev, weight_decay);
             sum += tmp;
         }
     return sum;
 }
 
-std::valarray<float> gauss_product(Mat image, Mat nlm_img, int y, int x, int conv_size, double stddev)
+std::valarray<float> gauss_product(Mat image, Mat nlm_img, int y, int x, int conv_size, double stddev, float weight_decay)
 {
     std::valarray<float> sum = {0, 0, 0};
 
@@ -58,7 +58,7 @@ std::valarray<float> gauss_product(Mat image, Mat nlm_img, int y, int x, int con
         for (int i = 0; i < nlm_img.rows; i++)
         {
             // f(p, q)
-            auto tmp = gauss_weight(nlm_img, y, x, j, i, conv_size, stddev);
+            auto tmp = gauss_weight(nlm_img, y, x, j, i, conv_size, stddev, weight_decay);
             tmp[0] *= (float)image.at<cv::Vec3b>(j, i)[0];
             tmp[1] *= (float)image.at<cv::Vec3b>(j, i)[1];
             tmp[2] *= (float)image.at<cv::Vec3b>(j, i)[2];
@@ -69,7 +69,7 @@ std::valarray<float> gauss_product(Mat image, Mat nlm_img, int y, int x, int con
     return sum;
 }
 
-Mat non_local_means_cpu(Mat image, int conv_size)
+Mat non_local_means_cpu(Mat image, int conv_size, float weight_decay)
 {
     auto nlm_img = image.clone();
     Scalar mean, stddev;
@@ -79,10 +79,10 @@ Mat non_local_means_cpu(Mat image, int conv_size)
         for (int i = 0; i < nlm_img.rows; i++)
         {
             //C(p)
-            auto cp_var = cp(image, j, i, conv_size, stddev_val);
+            auto cp_var = cp(image, j, i, conv_size, stddev_val, weight_decay);
 
             //sum(v(q) * f(p,q)
-            auto tmp = gauss_product(image, nlm_img, j, i, conv_size, stddev_val);
+            auto tmp = gauss_product(image, nlm_img, j, i, conv_size, stddev_val, weight_decay);
 
             //sum(v(q) * f(p,q) / C(p)
             tmp /= cp_var;
