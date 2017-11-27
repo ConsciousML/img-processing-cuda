ae6fe78a4c8c84a1871fcc4a82f3b08b0278a8f9
Axel Mendoza <axel.mendoza@epita.fr>
2017-11-27 11:25:46 +0100

    almost finish 3rd step of canny edge

--- src/cpu/convolution.cpp
+++ src/cpu/convolution.cpp
@@ -74,10 +74,74 @@ std::pair<double, double> conv_mask(cv::Mat image, int x, int y, int conv_size, 
     return std::pair<double, double>(res, d);
 }
 
+cv::Mat non_max_suppr(cv::Mat image, double *grad, double *dir, int threshold)
+{
+    for (int y = 0; y < image.cols; y++)
+    {
+        for (int x = 0; x < image.rows; x++)
+        {
+            double curr_dir = dir[x + y * image.rows];
+            double curr_grad = grad[x + y * image.rows];
+            if (22.5 <= curr_dir and curr_dir < 67.5
+                    and (x - 1) >= 0
+                    and (y + 1) < image.cols
+                    and (x + 1) < image.rows
+                    and (y - 1) >= 0)
+            {
+                double val1 = grad[(x - 1) + (y + 1) * image.rows];
+                double val2 = grad[(x + 1) + (y - 1) * image.rows];
+                if (val1 < curr_grad and val2 < curr_grad and curr_grad > 150)
+                    image.at<uchar>(x, y) = 255;
+                else
+                    image.at<uchar>(x, y) = 0;
+            }
+            else if (67.5 <= curr_dir and curr_dir < 112.5
+                    and (x - 1) >= 0
+                    and (x + 1) < image.rows)
+            {
+                double val1 = grad[(x - 1) + y * image.rows];
+                double val2 = grad[(x + 1) + y * image.rows];
+                if (val1 < curr_grad and val2 < curr_grad and curr_grad > 150)
+                    image.at<uchar>(x, y) = 255;
+                else
+                    image.at<uchar>(x, y) = 0;
+            }
+            else if (112.5 <= curr_dir and curr_dir < 157.5
+                    and (x - 1) >= 0
+                    and (y - 1) >= 0
+                    and (x + 1) < image.rows
+                    and (y + 1) < image.cols)
+            {
+                double val1 = grad[(x - 1) + (y - 1) * image.rows];
+                double val2 = grad[(x + 1) + (y + 1) * image.rows];
+                if (val1 < curr_grad and val2 < curr_grad and curr_grad > 150)
+                    image.at<uchar>(x, y) = 255;
+                else
+                    image.at<uchar>(x, y) = 0;
+            }
+            else if (((0 <= curr_dir and curr_dir < 22.5)
+                    or (157.5 <= curr_dir and curr_dir <= 180.0))
+                    and (y - 1) >= 0
+                    and (y + 1) < image.cols)
+            {
+                double val1 = grad[x + (y - 1) * image.rows];
+                double val2 = grad[x + (y + 1) * image.rows];
+                if (val1 < curr_grad and val2 < curr_grad and curr_grad > 150)
+                    image.at<uchar>(x, y) = 255;
+                else
+                    image.at<uchar>(x, y) = 0;
+            }
+        }
+    }
+    return image;
+}
+
 cv::Mat conv_with_mask(cv::Mat image, int conv_size)
 {
-    auto grad = image.clone();
-    auto direction = image.clone();
+
+    auto edge_image = image.clone();
+    double *grad = new double[image.cols * image.rows];
+    double *dir = new double[image.cols * image.rows];
     int mask1[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
     int mask2[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     for (int y = 0; y < image.cols; y++)
@@ -85,11 +149,11 @@ cv::Mat conv_with_mask(cv::Mat image, int conv_size)
         for (int x = 0; x < image.rows; x++)
         {
            auto pair = conv_mask(image, x, y, conv_size, mask1, mask2);
-           grad.at<uchar>(x, y) = pair.first;
-           std::cout << pair.second << std::endl;
-           direction.at<uchar>(x, y) = pair.second;
+           grad[x + y * image.rows] = pair.first;
+           dir[x + y * image.rows] = pair.second / 2;
         }
     }
-    return direction;
+    edge_image = non_max_suppr(edge_image, grad, dir);
+    return edge_image;
 }
 
